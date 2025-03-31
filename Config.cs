51c482b2a5738b12202d4119d96ef8aa7f8209f2
Noobest1001 <99349599+Noobest1001@users.noreply.github.com>
2025-03-31 09:36:38 -0400

    Formatting fixes

--- Config.cs
+++ Config.cs
@@ -8,9 +8,10 @@ namespace RoleSwap
     {
         public bool IsEnabled { get; set; } = true;
         public bool Debug { get; set; } = false;
+        
         [Description("The role that will be swapped when a detained NTF escapes")]
-
         public RoleTypeId NTFEscape = RoleTypeId.ChaosConscript;
+        
         [Description("The role that will be swapped when a detained Chaos Insurgency escapes")]
         public RoleTypeId CIEscape = RoleTypeId.NtfPrivate;
     }
